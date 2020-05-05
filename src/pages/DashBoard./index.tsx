08d3e6a3cc45595b48e616f6a22b9a048cbbdc88
jean <jeandub1@hotmail.com>
2020-05-05 18:32:46 -0400

    Initialize project using Create React App

---
+++ src/pages/DashBoard./index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Title } from './styles';
+
+const DashBoard: React.FC = () => {
+    return <Title>Explore Repository on GitHUb</Title>;
+};
+
+export default DashBoard;
---
+++ src/styles/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubIMage from '../assets/githubImage.svg';
+
+export default createGlobalStyle`
+*{
+    margin:0;
+
+    padding:0;
+    outline:0;
+    box-sizing:border-box;
+
+body{
+    background:#f0f0f5 url(${githubIMage}) no-repeat 70% top;
+
+    -webkit-font-smoothin:antialiased;
+}
+
+body,input,button{
+    font:16px Roboto, sans-serif
+
+}
+
+#root{
+    max-width:960px;
+    margin:0 auto;
+    padding:40px 20px;
+}
+button{
+    cursor: pointer;
+}
+}
+`;
